b0adc634fc2890f29c02d8ed2473003ee7afbaa5
Aurelia <aurelia-bot@aurelia.com>
2026-01-07 16:38:48 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,10 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add @tanstack/react-table@^8.15.0",
+    "bun add react-dropzone@^14.2.0"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
