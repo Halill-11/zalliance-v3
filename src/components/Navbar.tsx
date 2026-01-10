873e47318e6e25054455df617d63334b552948ca
Halill-11 <149505739+Halill-11@users.noreply.github.com>
2026-01-10 12:46:38 +0000

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,7 +46,7 @@ export function Navbar() {
           {<img 
                 src="zalliance.png" 
                 alt="ZALLIANCE" 
-                className="h-10 md:h-12 w-auto object-contain transition-transform duration-300 group-hover:scale-105"
+                className="h-14 md:h-12 w-auto object-contain transition-transform duration-300 group-hover:scale-105"
               />}
           <div className="flex-shrink-0">
             <a 
@@ -57,7 +57,7 @@ export function Navbar() {
               <img 
                 src="https://placehold.co/180x60/transparent/0F172A?text=ZALLIANCE" 
                 alt="ZALLIANCE" 
-                className="h-10 md:h-12 w-auto object-contain transition-transform duration-300 group-hover:scale-105"
+                className="h-18 md:h-12 w-auto object-contain transition-transform duration-300 group-hover:scale-105"
               />
             </a>
           </div>
